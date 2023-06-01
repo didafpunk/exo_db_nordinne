22932e6fdcd44b795ca28a2d59f36426ebb44339
Nordinne Ben Haddou El Khattabi <nordinnebenhaddou@hotmail.com>
2023-06-01 20:36:11 +0200

    start project

---
+++ src/main/java/com/prid/exercice_db_nordinne/DbNordinneApplication.java
@@ -0,0 +1,13 @@
+package com.prid.exercice_db_nordinne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbNordinneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbNordinneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prid/exercice_db_nordinne/DbNordinneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prid.exercice_db_nordinne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbNordinneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
